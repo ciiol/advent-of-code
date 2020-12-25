8dde5d00c2e5f3326277423bcb0bfec0107f3e9d
Andrey Fadeev <me@ciiol.net>
2020-12-25 11:48:53 +0500

    Add 2020 day 24

---
+++ 2020/day24/puzzle1.hs
@@ -0,0 +1,73 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i runhaskell -p
+
+-- https://adventofcode.com/2020/day/24
+-- A member of the renovation crew gives you a list of the tiles
+-- that need to be flipped over (your puzzle input). Each line in
+-- the list identifies a single tile that needs to be flipped by
+-- giving a series of steps starting from a reference tile in the
+-- very center of the room. (Every line starts from the same
+-- reference tile.)
+
+-- Go through the renovation crew's list and determine which tiles
+-- they need to flip. After all of the instructions have been followed,
+-- how many tiles are left with the black side up?
+
+import qualified Data.List as L
+import qualified Data.Set as S
+import Text.ParserCombinators.Parsec (Parser, many1, endBy1, parse, try, string, newline, (<|>))
+import System.Exit (exitFailure)
+
+data Step = E | SE | SW | W | NW | NE deriving (Show, Eq, Ord)
+type Coord = (Int, Int, Int)
+
+tryString :: String -> Parser String
+tryString = try . string
+
+stopParser :: Parser Step
+stopParser =
+    tryString "e" *> pure E
+    <|> tryString "se" *> pure SE
+    <|> tryString "sw" *> pure SW
+    <|> tryString "w" *> pure W
+    <|> tryString "nw" *> pure NW
+    <|> tryString "ne" *> pure NE
+
+toCoord :: Step -> Coord
+toCoord E = (1, -1, 0)
+toCoord W = (-1, 1, 0)
+toCoord SE = (0, -1, 1)
+toCoord SW = (-1, 0, 1)
+toCoord NE = (1, 0, -1)
+toCoord NW = (0, 1, -1)
+
+addCoord :: Coord -> Coord -> Coord
+addCoord (x, y, z) (dx, dy, dz) = (x + dx, y + dy, z + dz)
+
+walk :: [Step] -> Coord
+walk = L.foldl' walk' (0, 0, 0)
+  where
+    walk' acc = (addCoord acc) . toCoord
+
+flippedTiles :: [Coord] -> [Coord]
+flippedTiles = S.toList . L.foldl' flippedTiles' S.empty
+  where
+    flippedTiles' acc pos | S.member pos acc = S.delete pos acc
+    flippedTiles' acc pos | S.notMember pos acc = S.insert pos acc
+    flippedTiles' _ _ = error "oops"
+
+
+parseInput :: Parser a -> IO a
+parseInput parser = parseInput' parser >>= either report return
+  where
+    report err = do
+      putStrLn $ "Error: " ++ show err
+      exitFailure
+    parseInput' p = do
+      input <- getContents
+      return $ parse p "(stdin)" input
+
+main :: IO ()
+main = do
+  paths <- parseInput $ endBy1 (many1 stopParser) newline
+  putStrLn $ show $ length $ flippedTiles $ map walk paths
