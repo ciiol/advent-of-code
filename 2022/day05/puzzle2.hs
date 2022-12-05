477d9b13fe483be152f485dcf6d984ecb072e1f6
Andrey Fadeev <me@ciiol.net>
2022-12-05 22:32:26 +0100

    Add 2020 day 5

---
+++ 2022/day05/puzzle2.hs
@@ -0,0 +1,84 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i runhaskell -p
+
+-- https://adventofcode.com/2022/day/5#part2
+-- Your puzzle input is a drawing of the starting stacks of crates
+-- and the rearrangement procedure.
+
+-- The CrateMover 9001 is notable for many new and exciting features: air conditioning,
+-- leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+-- After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+import Text.ParserCombinators.Parsec (Parser, sepBy1, sepEndBy1, try, alphaNum,
+                                      char, parse, string, (<|>), digit, many1)
+import System.Exit (exitFailure)
+import Data.List (transpose, foldl')
+import Data.Maybe (catMaybes, fromMaybe)
+import Data.Map.Strict as M (Map, fromList, adjust, lookup, elems)
+
+type Crate = Char
+type Stack = [Crate]
+type Index = Int
+type Cargo = M.Map Index Stack
+data Instruction = Move Int Index Index deriving (Show)
+
+cargoParser :: Parser Cargo
+cargoParser = makeCargo <$> sepEndBy1 (try line) (char '\n') <*> indexesParser
+  where
+    line = sepBy1 (parseCrate <|> parseNoCrate) (char ' ')
+
+indexesParser :: Parser [Index]
+indexesParser = char ' ' *> sepBy1 indexParser (try $ string "   ") <* string " \n"
+
+indexParser :: Parser Index
+indexParser = read <$> many1 digit
+
+parseCrate :: Parser (Maybe Crate)
+parseCrate = Just <$> (char '[' *> alphaNum <* char ']')
+
+parseNoCrate :: Parser (Maybe Crate)
+parseNoCrate = do
+  _ <- string "   "
+  return Nothing
+
+makeCargo :: [[Maybe Crate]] -> [Index] -> Cargo
+makeCargo lines indexes = M.fromList $ zip indexes stacks 
+  where
+    stacks = map catMaybes $ transpose lines
+
+instructionParser :: Parser Instruction
+instructionParser = do
+  _ <- string "move "
+  num <- read <$> many1 digit
+  _ <- string " from "
+  from <- indexParser
+  _ <- string " to "
+  to <- indexParser
+  return $ Move num from to
+
+instructionsParser :: Parser [Instruction]
+instructionsParser = sepEndBy1 instructionParser (char '\n')
+
+inputParser :: Parser (Cargo, [Instruction])
+inputParser = (,) <$> cargoParser <*> (char '\n' *> instructionsParser)
+
+apply :: Cargo -> Instruction -> Cargo
+apply cargo (Move num from to) = newCargo
+  where
+    elems = take num stackFrom
+    stackFrom = fromMaybe (error "no from") $ M.lookup from cargo
+    newCargo = M.adjust (elems ++) to $ M.adjust (drop num) from cargo
+
+parseInput :: Parser a -> IO a
+parseInput parser = parseInput' parser >>= either report return
+  where
+    report err = do
+      putStrLn $ "Error: " ++ show err
+      exitFailure
+    parseInput' p = do parse p "(stdin)" <$> getContents
+
+main :: IO ()
+main = do
+  (cargo, instructions) <- parseInput inputParser
+  print $ map head $ M.elems $ foldl' apply cargo instructions
